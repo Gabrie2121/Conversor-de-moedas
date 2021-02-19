4cd8a9c6702420325112f332c2870db60c8dc95a
Gabrie2121 <gabriel.miranda041200@gmail.com>
2021-02-19 15:45:49 -0300

    Finalizado a conversão de real em dolar

--- assets/scripts/convert.js
+++ assets/scripts/convert.js
@@ -1,11 +1,16 @@
-const dolar = fetch('https://economia.awesomeapi.com.br/json/USD-BRL').then(resposta => {
-    return resposta.json()
-}).then(data=>{
-    console.log(data)
-    console.log(data[0].code)//dolar
-    console.log(data[0].bid)//valor
-    
-} 
-).catch(e => {
-    console.log(e)
-})
+//conexão com a api do awesome API
+const api =async () =>{
+    try{
+        const dolar = await fetch('https://economia.awesomeapi.com.br/json/USD-BRL').then(resposta => {
+            return resposta.json()
+        }).then(data=>{
+            return data
+        } 
+        ).catch(e => {
+            console.error(e)
+        })
+        return dolar[0]
+    }catch{
+        alert(Error('Estou com problemas, por favor tente novamente mais tarde'))
+    }
+}
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,35 @@
 const val = document.querySelector('#valor')
 const btn = document.querySelector('button')
-const check = (e)=>{
-    return typeof(e)==Number?True:false
+const result = document.querySelector('#resultado')
+const check = (e) => {
+    return typeof (e) == Number ? True : false
 }
-btn.addEventListener('click',e=>{
-
+const pressBtn = btn.addEventListener('click', e => {
+    event.preventDefault()
+    if(val.value == ''){
+        showError('Please insert a value')
+    }else{
+        chamada()
+    }
+    
 })
+val.addEventListener('keypress', e => {
+    pressBtn
+    if (e.keyCode == 13) pressBtn
+})
+const chamada = async () => {
+    const ap = await api()
+    calcularValor(ap)
+    console.log(ap)
+}
+const calcularValor = v => {
+    const total = val.value/v.bid
+    const type = v.code
+    showValue(total,type)
+}
+const showValue = (x,text) =>{
+    result.innerHTML=`${text} ${x.toFixed(2)}`
+}
+const showError = (x)=>{
+    result.innerHTML = `<span style="color:red;">${x}<span/>`.toUpperCase()
+}
